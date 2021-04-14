6ef310ec1db29fdb75d1a0d744906650f8a34288
1bertomelo <1bertomelo@gmail.com>
2021-04-14 20:16:00 -0300

    correcao bug validações

--- src/validations/AlunoValidations.js
+++ src/validations/AlunoValidations.js
@@ -13,7 +13,7 @@ const AlunoValidationRules = () => {
         body('nome').isLength({ min: 2, max: 100 }).withMessage('Tamanho do nome deve ser entre 2 e 100'),
         body('senha').not().isIn(['123', 'password', 'god'])
             .withMessage('A senha não pode ser palavras comuns')
-            .notEmpty().isLength({ min: 8 }).withMessage('Senha deve ser obrigatório')
+            .notEmpty().isLength({ min: 8 }).withMessage('Senha deve ser conter no mínimo 8 caracteres')
     ]
 }
 
